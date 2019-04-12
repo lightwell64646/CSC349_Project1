a4068948086eaf9d9404adc0043600385ccf2ba0
Nathan Pestes <npestes@calpoly.edu>
2019-04-12 08:31:58 -0700

    Needs headers needs final check

--- SortCounts.java
+++ SortCounts.java
@@ -1,18 +1,14 @@
 import java.util.Random;
 import java.util.Arrays;
 
-class SortTimes{
+class SortCounts{
    public interface my_sort_interface{
       public void sort(int[] arr, int N);
    }
 
    public static void main(String []args){
       Random rd = new Random();
-      Sorts mysorts = new Sorts1();
-
-      my_sort_interface selectS = (arr, N) -> mysorts.selectionSort(arr, N);
-      my_sort_interface mergeS = (arr, N) -> mysorts.mergeSort(arr, N);
-      my_sort_interface quickS = (arr, N) -> mysorts.quickSort(arr, N);
+      Sorts1 mysorts = new Sorts1();
 
       for (int N = 5000; N <= 160000; N *= 2){
          long qs_time = 0;
@@ -30,9 +26,9 @@ class SortTimes{
                arr3[j] = arr1[j];
                arr4[j] = arr1[j];
             }
-            ss_time = selectS.sort(arr1, N);
-            ms_time = mergeS.sort(arr2, N);
-            qs_time = quickS.sort(arr3, N);
+            ss_time = mysorts.selectionSort(arr1, N);
+            ms_time = mysorts.mergeSort(arr2, N);
+            qs_time = mysorts.quickSort(arr3, N);
             System.out.printf("N = %d C_ss =  %d, C_ms = %d, C_qs = %d\n",  N, ss_time, ms_time, qs_time);
          }
          System.out.printf("\n");
